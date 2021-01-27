caa82bc6260d9ae48cbd2206673abdd52c074207
Jhaniel <jhaart3@gmail.com>
2021-01-26 20:46:37 -0600

    A

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Proyecto Python MySQL:
+-Abrir asistente
+-Login o Registro
+|-Registro: Creara un usuario en la base de datos.
+|-Login: identificara al usuario y no dara más opciones.
+#Crear notas, mostrar notas, borrar notas.
+"""
